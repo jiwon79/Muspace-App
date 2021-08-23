0881c00526e383ef6c74e412ea7df70f5d134eb4
jiwon79 <19-079@ksa.hs.kr>
2021-08-24 03:20:25 +0900

    fix: Fix vw, vh unit on Planet components

--- components/Planet.js
+++ components/Planet.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import {vw, vh, vmin, vmax} from 'react-native-expo-viewport-units';
 
 export default function Planet() {
+
   return (
     <View style={styles.planet}></View>
   )
@@ -12,13 +14,13 @@ const styles = StyleSheet.create({
     position: 'absolute',
     top: '50%',
     left: '50%',
-    height: '120vw',
-    width: '120vw',
+    height: vw(120),
+    width: vw(120),
     transform: [
-      { translateY: '-50%' },
-      { translateX: '-50%' }
+      { translateY: vw(-60) },
+      { translateX: vw(-60) }
     ],
-    borderRadius: '50%',
+    borderRadius: vw(60),
     backgroundColor: '#aaaaaa'
   }
 })
