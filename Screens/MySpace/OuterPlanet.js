d55e375c1853a0c2eefd6e8baed9679316b158d2
jiwon79 <19-079@ksa.hs.kr>
2021-08-24 22:57:24 +0900

    feat: Add Music Thumnain comp

--- Screens/MySpace/OuterPlanet.js
+++ Screens/MySpace/OuterPlanet.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { View, Text, Button } from 'react-native'
 
 import Planet from './../../components/Planet';
+import MusicThum from '../../components/MusicThum';
 
 export default function OuterPlanet({ navigation }) {
   return (
     <View>
       <Text>planet outer</Text>
       <Planet/>
+      <MusicThum navigation={navigation}/>
       <Button
       title="in planet"
       onPress={() => navigation.navigate("OnPlanet")}
---
+++ components/MusicThum.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import { View, Text, StyleSheet, ImageBackground, TouchableOpacity  } from 'react-native'
+import {vw, vh, vmin, vmax} from 'react-native-expo-viewport-units';
+
+export default function MusicThum({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <ImageBackground source={require("../assets/snoo.jpg")} style={styles.bgImage}/>
+      <TouchableOpacity
+        activeOpacity={0.8}
+        onPress={() => navigation.navigate('OnPlanet')}
+        style={styles.button}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    top: vh(50),
+    left: vw(50),
+    height: vw(70),
+    width: vw(70),
+    transform: [
+      { translateY: vw(-35) },
+      { translateX: vw(-35) }
+    ]
+  },
+  bgImage: {
+    width: '100%', 
+    height: '100%',
+    borderRadius: vw(45),
+    overflow: 'hidden'
+  },
+  button: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    width: 0,
+    height: 0,
+    backgroundColor: "transparent",
+    borderStyle: "solid",
+    borderLeftWidth: 30,
+    borderRightWidth: 30,
+    borderBottomWidth: 60,
+    borderLeftColor: "transparent",
+    borderRightColor: "transparent",
+    transform: [
+      { translateX: -30 },
+      { translateY: -30 },
+      { rotateZ: '90deg'}
+    ],
+    borderBottomColor: "blue",
+  }
+})
