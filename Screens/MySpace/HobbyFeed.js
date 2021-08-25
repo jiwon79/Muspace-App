148da7a71ded88d35e01c7bd24e1e89d2c8446e2
jiwon79 <19-079@ksa.hs.kr>
2021-08-25 12:09:01 +0900

    feat: Add hobby feed params

--- Screens/MySpace/HobbyFeed.js
+++ Screens/MySpace/HobbyFeed.js
@@ -1,10 +1,17 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 
-export default function HobbyFeed() {
+export default function HobbyFeed({ navigation: { goBack }, route }) {
+  console.log(route)
+  const hobby = route.params.hobby;
+
   return (
     <View>
-      <Text>Hobby Feed</Text>
+      <Button 
+        title="go back"
+        onPress={()=>goBack()}
+        />
+      <Text>{hobby} Feed</Text>
     </View>
   )
 }
