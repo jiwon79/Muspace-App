fc1179791961be7e11395fe9a139c593da3c72c1
jiwon79 <19-079@ksa.hs.kr>
2021-08-25 12:41:08 +0900

    feat: Add Planet scale animatoin

--- Screens/MySpace/InPlanet.js
+++ Screens/MySpace/InPlanet.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 import { View, Text, Button, FlatList, Animated } from 'react-native'
 
 import Planet from '../../components/Planet';
@@ -7,10 +7,28 @@ import Hobby from '../../components/Hobby';
 const hobbyList = ['취미', '운동', '음악', '글귀', '여행', '책'];
 
 export default function InPlanet({ navigation }) {
+  const SacleAnim = useRef(new Animated.Value(1)).current
+
+  useEffect(() => {
+    Animated.timing(
+      SacleAnim,
+      {     
+        toValue: 2,
+        duration: 1000,
+        useNativeDriver: true
+      }
+    ).start();
+  }, [SacleAnim])
+
   return (
     <View>
       <Text>on planet</Text>
-      <Planet/>
+      <Animated.View
+        style={{ transform: [{ scale: SacleAnim }]}}
+      >
+        <Planet/>
+      </Animated.View>
+      
       <Button
       title="Outer Planet"
       onPress={() => navigation.navigate('OuterPlanet')}
