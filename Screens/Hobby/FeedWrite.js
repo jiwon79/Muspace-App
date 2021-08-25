ed9ba191a4429b6cf96f49f74d06ec4775d550fd
jiwon79 <19-079@ksa.hs.kr>
2021-08-26 03:19:28 +0900

    feat: Add title, content input on feed write page

--- Screens/Hobby/FeedWrite.js
+++ Screens/Hobby/FeedWrite.js
@@ -1,10 +1,12 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native'
 
 import HobbyContentWriteHeader from '../../components/Header/HobbyContentWriteHeader';
 
 export default function FeedWrite({ navigation, route }) {
   const hobby = route.params.hobby;
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
 
   return (
     <View>
@@ -12,8 +14,43 @@ export default function FeedWrite({ navigation, route }) {
         navigation={navigation}
         hobby={hobby}
       />
-      <Text>{hobby}</Text>
-      <Text>feed write</Text>
+      <Text style={styles.text}>feed write</Text>
+      <TextInput
+        onChangeText={setTitle}
+        value={title}
+        returnKeyType="next"
+        placeholder="제목"
+        style={styles.titleInput}
+      />
+      <TextInput
+        onChangeText={setContent}
+        value={content}
+        placeholder="내용"
+        style={styles.contentInput}
+      />
+      <TouchableOpacity style={styles.submitButton}>
+        <Text style={styles.text}>Submit</Text>
+      </TouchableOpacity>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center'
+  },
+  text: {
+    textAlign: 'center'
+  },
+  titleInput: {
+    fontSize: 25
+  },
+  contentInput: {
+    fontSize: 20
+  },
+  submitButton: {
+    width: '40%',
+    height: 30,
+    backgroundColor: '#eeaadd'
+  }
+})
