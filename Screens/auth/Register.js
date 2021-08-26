6931bfa1b315e6eeb1d56206c0056bb8d0371dfe
jiwon79 <19-079@ksa.hs.kr>
2021-08-27 02:11:18 +0900

    feat: Add Rigesiter page styles

--- Screens/auth/Register.js
+++ Screens/auth/Register.js
@@ -1,10 +1,18 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 export default function Register() {
   return (
     <View>
-      <Text>Register page</Text>
+      <Text style={styles.title}>Register page</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 30,
+    textAlign: 'center',
+    marginTop: 20
+  }
+})
