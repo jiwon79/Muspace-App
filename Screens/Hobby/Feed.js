23f147afba41ca0f97f4bd8b2b9cdd935c1f1dc8
jiwon79 <19-079@ksa.hs.kr>
2021-08-26 20:11:05 +0900

    feat: Add Image content slider

--- Screens/Hobby/Feed.js
+++ Screens/Hobby/Feed.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { View, Text } from 'react-native'
-
+import { View, Text, StyleSheet } from 'react-native'
+import PagerView from 'react-native-pager-view';
 import HobbyFeedHeader from '../../components/Header/HobbyFeedHeader';
 import HobbyContent from '../../components/HobbyContent';
 
@@ -9,16 +9,43 @@ export default function Feed({ navigation, route }) {
   const hobby = route.params.hobby;
 
   return (
-    <View>
+    <View style={styles.container}>
       <HobbyFeedHeader 
         hobby={hobby}
         navigation={navigation}
       />
-      <HobbyContent
-        imageURL='https://reactjs.org/logo-og.png'
-        title='제목제목'
-        content='내용내용'
-      />
+      <PagerView style={styles.viewPager} initialPage={1}>
+        <View key="1">
+          <HobbyContent
+            imageURL='https://reactjs.org/logo-og.png'
+            title='제목제목'
+            content='내용내용'
+          />
+        </View>
+        <View key="2">
+          <HobbyContent
+            imageURL='https://reactjs.org/logo-og.png'
+            title='제목제목'
+            content='내용내용'
+          />
+        </View>
+        <View key="3">
+          <HobbyContent
+            imageURL='https://reactjs.org/logo-og.png'
+            title='제목제목'
+            content='내용내용'
+          />
+        </View>
+      </PagerView>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  viewPager: {
+    flex: 1,
+  }
+});
