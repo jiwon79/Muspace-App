4345e94ffa529f75af9c5165ebb0b84858b736d1
jiwon79 <19-079@ksa.hs.kr>
2021-08-26 20:56:31 +0900

    feat: Add feed slider arrow button

--- Screens/Hobby/Feed.js
+++ Screens/Hobby/Feed.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import React, { useState, useRef } from 'react'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 import PagerView from 'react-native-pager-view';
 import { AntDesign } from '@expo/vector-icons'; 
 
@@ -7,8 +7,11 @@ import HobbyFeedHeader from '../../components/Header/HobbyFeedHeader';
 import HobbyContent from '../../components/HobbyContent';
 
 export default function Feed({ navigation, route }) {
-  console.log(route)
   const hobby = route.params.hobby;
+  const [activeIndex, setActiveIndex] = useState(0);
+  const pager = useRef(0);
+  // api 받을 때 갯수 세서 넣어줘야됨
+  const feedNum = 3;
 
   return (
     <View style={styles.container}>
@@ -17,8 +20,18 @@ export default function Feed({ navigation, route }) {
         navigation={navigation}
       />
       <View style={styles.contentContainer}>
-        <AntDesign name="left" color="black" style={styles.arrowIcon}/>
+        <TouchableOpacity
+          onPress={() => {
+            if (activeIndex != 0)
+              pager.current.setPage(activeIndex-1)
+          }}
+        >
+          <AntDesign name="left" color="black" style={styles.arrowIcon}/>
+        </TouchableOpacity>
+
         <PagerView
+          ref={pager}
+          onPageSelected={({ nativeEvent }) => setActiveIndex(nativeEvent.position)}
           style={styles.viewPager} 
           initialPage={0}
           showPageIndicator={true}
@@ -26,26 +39,34 @@ export default function Feed({ navigation, route }) {
           <View key="1">
             <HobbyContent
               imageURL='https://reactjs.org/logo-og.png'
-              title='제목제목'
-              content='내용내용'
+              title='제목1'
+              content='내용1'
             />
           </View>
           <View key="2">
             <HobbyContent
               imageURL='https://reactjs.org/logo-og.png'
-              title='제목제목'
-              content='내용내용'
+              title='제목2'
+              content='내용2'
             />
           </View>
           <View key="3">
             <HobbyContent
               imageURL='https://reactjs.org/logo-og.png'
-              title='제목제목'
-              content='내용내용'
+              title='제목3'
+              content='내용3'
             />
           </View>
         </PagerView>
-        <AntDesign name="right" color="black" style={styles.arrowIcon}/>
+
+        <TouchableOpacity
+          onPress={() => {
+            if (activeIndex != feedNum-1)
+              pager.current.setPage(activeIndex+1)
+          }}
+        >
+          <AntDesign name="right" color="black" style={styles.arrowIcon}/>
+        </TouchableOpacity>
       </View>
     </View>
   )
