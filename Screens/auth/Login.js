f0eaa40a981145255ea9540f7caedc3c52399083
jiwon79 <19-079@ksa.hs.kr>
2021-08-27 02:19:11 +0900

    feat: Add login page styles

--- Screens/auth/Login.js
+++ Screens/auth/Login.js
@@ -1,23 +1,48 @@
 import React from 'react'
-import { View, Text, Button, TextInput } from 'react-native'
+import { View, Text, Button, TextInput, StyleSheet } from 'react-native'
+
+import ButtonModule from '../../components/atom/ButtonModule';
 
 export default function Login({ route, navigation }) {
   const { handleLogged, otherParam } = route.params;
 
   return (
     <View>
-      <Text>Login page</Text>
+      <Text style={styles.title}>Login page</Text>
       <TextInput
         placeholder="name"
+        style={styles.inputLogin}
       />
       <TextInput
         placeholder="password"
+        secureTextEntry={true}
+        style={styles.inputPassword}
       />
       
-      <Button
-        title="login"
+      <ButtonModule
+        text="login"
         onPress = {() => handleLogged()}
       />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 25,
+    textAlign: 'center',
+    marginTop: 20
+  },
+  inputLogin: {
+    marginLeft: '8%',
+    marginRight: '8%',
+    fontSize: 15,
+    marginBottom: 15
+  },
+  inputPassword: {
+    marginLeft: '8%',
+    marginRight: '8%',
+    fontSize: 15,
+    marginBottom: 30
+  }
+})
