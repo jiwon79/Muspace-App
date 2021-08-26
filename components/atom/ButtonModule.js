4725772ed5ebaad8d473d0ba024dc1c9d1f38f6a
jiwon79 <19-079@ksa.hs.kr>
2021-08-27 01:43:35 +0900

    feat: Add button module

---
+++ components/atom/ButtonModule.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { View, Text, Button, StyleSheet, TouchableOpacity } from 'react-native'
+
+export default function ButtonModule(props) {
+  return (
+    <TouchableOpacity
+      onPress={props.onPress}
+      style={[styles.button, props.style]}
+    >
+      <Text style={styles.text}>{props.text}</Text>
+    </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+  button: {
+    alignSelf: 'center',
+    justifyContent: 'center',
+    width: '60%',
+    height: 40,
+    borderWidth: 2,
+    borderColor: '#666666',
+    borderRadius: 20,
+    backgroundColor: '#fff'
+  },
+  text: {
+    fontSize: 18,
+    color: '#000',
+    textAlign: 'center',
+  }
+})
