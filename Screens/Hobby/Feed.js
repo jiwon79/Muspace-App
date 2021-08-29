048f9efa31d0be28f69f33d1a334af60e22d023a
jiwon79 <19-079@ksa.hs.kr>
2021-08-29 22:58:17 +0900

    feat: Add temp image on Feed

--- Screens/Hobby/Feed.js
+++ Screens/Hobby/Feed.js
@@ -3,6 +3,8 @@ import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 import PagerView from 'react-native-pager-view';
 import { AntDesign } from '@expo/vector-icons'; 
 import axios from "axios";
+import Base64ArrayBuffer from 'base64-arraybuffer';
+import Base64 from 'Base64'
 
 import HobbyFeedHeader from '../../components/Header/HobbyFeedHeader';
 import HobbyContent from '../../components/HobbyContent';
@@ -17,33 +19,82 @@ export default function Feed({ navigation, route }) {
   // api 받을 때 갯수 세서 넣어줘야됨
   const [feedNum, setFeedNum] = useState(0);
   const [feedComponents, setFeedComponents] = useState(null);
-  const url = APIURL+'get_post?category='+hobby;
+
+
+  const imageFetchData = async (id) => {
+    return axios.get(APIURL+'get_image?post_id='+id, {
+      responseType: 'arraybuffer'
+    })
+    .then(response => {
+      const base64 = Base64.btoa(
+        new Uint8Array(response.data).reduce(
+          (data, byte) => data + String.fromCharCode(byte),
+          '',
+        ),
+      );
+      console.log(base64)
+      return ("data:image/png;base64," + base64);
+    });
+    // return imageResponse
+  }
+
+  const fetchFeedData = async () => {
+    const feedFetchData = await axios.get(APIURL+'get_post?category='+hobby)
+    .then(function (response) {
+      return response.data
+    })
+    .catch(function (error) {
+      console.log(error);
+      console.log("error");
+    });
+
+
+    console.log('start')
+    console.log(feedFetchData)
+    const feedModifyData = feedFetchData.map((feed) => {
+      const imageURI = axios.get(APIURL+'get_image?post_id='+feed.id, {
+        responseType: 'arraybuffer'
+      })
+      .then(response => {
+        const base64 = Base64.btoa(
+          new Uint8Array(response.data).reduce(
+            (data, byte) => data + String.fromCharCode(byte),'',
+          ),
+        );
+        return ("data:image/png;base64," + base64);
+      });
+      console.log(imageURI);
+      // console.log('image uri')
+      // console.log(imageURI._W)
+      return ({
+        ...feed,
+        imageURL: 'http://t1.daumcdn.net/friends/prod/editor/dc8b3d02-a15a-4afa-a88b-989cf2a50476.jpg'
+        // imageURL: imageURI
+      })
+    })
+    console.log('end')
+    console.log(feedModifyData)
+    setFeedList(feedModifyData)
+  }
 
   useEffect(() => {
-    const fetchFeedData = async () => {
-      await axios.get(url)
-      .then(function (response) {
-        setFeedList(response.data)
-      })
-      .catch(function (error) {
-        console.log(error);
-        console.log("error");
-      });
-    }
     fetchFeedData();
   }, []);
 
   useEffect(() => {
     if (feedList != null) {
       setFeedNum(feedList.length);
-      const components = feedList.map((feed, index) =>(
-        <View key={index+1}>
-          <HobbyContent
-            title={feed.title}
-            content={feed.contents}
-          />
-        </View>
-      ));
+      const components = feedList.map((feed, index) =>{
+        console.log(feed)
+        return (
+          <View key={index+1}>
+            <HobbyContent
+              title={feed.title}
+              content={feed.contents}
+              imageURL={feed.imageURL}
+            />
+          </View>
+        )});
       setFeedComponents(components);
       pager.current.setPage(activeIndex)
     }
@@ -65,6 +116,7 @@ export default function Feed({ navigation, route }) {
         >
           <AntDesign name="left" color="black" style={styles.arrowIcon}/>
         </TouchableOpacity>
+        {feedNum == 0 ? <Text style={styles.noticeText}>게시글이 없습니다.</Text> : <Text></Text>}
         <PagerView
           ref={pager}
           onPageSelected={({ nativeEvent }) => setActiveIndex(nativeEvent.position)}
@@ -96,6 +148,10 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'row',
     alignItems: 'center'
+  },
+  noticeText: {
+    fontSize: 24,
+    textAlign: 'center',
   },
   viewPager: {
     flex: 1,
