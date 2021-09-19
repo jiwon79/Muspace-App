affe00f1aeb91b83ab6e41ce77fbc50a35b0aa59
jiwon79 <19-079@ksa.hs.kr>
2021-09-20 01:18:02 +0900

    feat: Add FindPw page content

--- Screens/auth/FindPw.js
+++ Screens/auth/FindPw.js
@@ -1,10 +1,54 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import { Hoshi } from 'react-native-textinput-effects';
+
+import ButtonModule from '../../components/atom/ButtonModule';
 
 export default function FindPw() {
+  const [id, setId] = useState('');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
   return (
     <View>
-      <Text>Find password page</Text>
+      <Hoshi
+        label={'아이디'}
+        borderColor={'#b76c94'}
+        borderHeight={3}
+        inputPadding={16}
+        onChangeText={setId}
+        style={styles.textInput}
+      />
+
+      <Hoshi
+        label={'성명'}
+        borderColor={'#b76c94'}
+        borderHeight={3}
+        inputPadding={16}
+        onChangeText={setName}
+        style={styles.textInput}
+      />
+
+      <Hoshi
+        label={'이메일'}
+        borderColor={'#b76c94'}
+        borderHeight={3}
+        inputPadding={16}
+        onChangeText={setName}
+        style={styles.textInput}
+      />
+
+      <ButtonModule 
+        text={'확인'}
+      />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  textInput: {
+    marginLeft: '10%',
+    marginRight: '10%',
+    marginBottom: '5%'
+  }
+})
