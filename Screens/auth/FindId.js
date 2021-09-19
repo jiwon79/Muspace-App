e5d13e0daf8d4fc593e31c627e3a2519faf689de
jiwon79 <19-079@ksa.hs.kr>
2021-09-19 18:55:13 +0900

    feat: Add FindId page content

--- Screens/auth/FindId.js
+++ Screens/auth/FindId.js
@@ -1,10 +1,44 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, StyleSheet, Button } from 'react-native'
+import { Hoshi } from 'react-native-textinput-effects';
+
+import ButtonModule from '../../components/atom/ButtonModule';
 
 export default function FindID() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
   return (
     <View>
-      <Text>Find id page</Text>
+      <Hoshi
+        label={'성명'}
+        borderColor={'#b76c94'}
+        borderHeight={3}
+        inputPadding={16}
+        onChangeText={setName}
+        style={styles.textInput}
+      />
+
+      <Hoshi
+        label={'이메일'}
+        borderColor={'#b76c94'}
+        borderHeight={3}
+        inputPadding={16}
+        onChangeText={setEmail}
+        style={styles.textInput}
+      />
+
+      <ButtonModule 
+        text={'확인'}
+      />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  textInput: {
+    marginLeft: '10%',
+    marginRight: '10%',
+    marginBottom: '5%'
+  }
+})
