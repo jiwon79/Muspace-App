7be9e8eb731015c9426965910d1a34399fe0553b
jiwon79 <19-079@ksa.hs.kr>
2021-09-21 20:15:37 +0900

    feat: Add SelectGalaxyHeader styles

--- components/Header/SelectGalaxyHeader.js
+++ components/Header/SelectGalaxyHeader.js
@@ -1,10 +1,30 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
+import { FontAwesome5 } from '@expo/vector-icons';
 
 export default function SelectGalaxyHeader() {
   return (
-    <View>
-      <Text>갤럭시를 선택하세요!</Text>
+    <View style={styles.container}>
+      <FontAwesome5 name="space-shuttle" style={styles.icon} />
+      <Text style={styles.text}>갤럭시를 선택하세요!</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignSelf: 'center',
+    borderWidth: 2,
+    borderRadius: 10,
+    padding: 20,
+    marginTop: 20
+  },
+  icon: {
+    fontSize: 30,
+    color: 'black'
+  },
+  text: {
+    fontSize: 30
+  }
+})
