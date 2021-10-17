eeb4bb4213c669f25ec789b0b09c4aad619900f3
jiwon79 <19-079@ksa.hs.kr>
2021-10-18 04:12:53 +0900

    feat: Change Header icon, styles

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,33 +1,41 @@
 import React from 'react'
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
-import { Ionicons } from '@expo/vector-icons';
-import { Feather } from '@expo/vector-icons';
+import { View, Text, TouchableOpacity, Image } from 'react-native'
+import EStyleSheet from 'react-native-extended-stylesheet';
+
+import styleGuide from '../../utils/styleGuide';
+import Discovery from './../../assets/icon/Discovery_white.svg'
+import More from './../../assets/icon/More_white.svg'
 
 export default function Header({ navigation }) {
   return (
     <View style={styles.container}>
-      <Ionicons name='musical-note' style={styles.icon}/>
-      <Text style={styles.text}>이름</Text>
+      <TouchableOpacity
+        onPress={() => console.log('press')}
+        >
+        <Discovery/>
+      </TouchableOpacity>
+      
+      <Text style={[styleGuide.display_04, styles.title]}>
+        기셔님의 행성
+      </Text>
+      
       <TouchableOpacity
         onPress={() => navigation.openDrawer()}
       >
-        <Feather 
-          name='more-vertical' 
-          style={styles.icon}
-        />
+        <More/>
       </TouchableOpacity>
     </View>
   )
 }
 
-const styles = StyleSheet.create({
+const styles = EStyleSheet.create({
   container: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    padding: 10,
+    padding: 20
   },
-  icon: {
-    fontSize: 24
+  title: {
+    color: '#fff'
   },
   text: {
     fontSize: 20
