cbcd6dd5b56f88df060948f47942d17f8b1d38b8
jiwon79 <19-079@ksa.hs.kr>
2021-10-24 03:51:57 +0900

    feat: Apply typescript on OuterPlanet screen

--- Screens/MySpace/OuterPlanet.tsx
+++ Screens/MySpace/OuterPlanet.tsx
@@ -1,13 +1,15 @@
-import React, { useRef, useState, Component } from 'react'
+import React, { useRef, useState, ReactElement } from 'react'
 import { View, Text, Button, Animated } from 'react-native'
 import EStyleSheet from 'react-native-extended-stylesheet';
 
+import {MySpaceOuterPlanetProp} from "../../utils/types";
+
 import Header from '../../components/Header/Header';
-import Planet from '../../components/MySpace/Planet';
-import MusicThum from '../../components/MySpace/MusicThum';
+import Planet from '../../components/MySpace/Planet/Planet';
+import MusicThum from '../../components/MySpace/Planet/MusicThum';
 import MusicDesc from '../../components/MySpace/MusicDesc';
 
-export default function OuterPlanet({ route, navigation }) {
+export default function OuterPlanet({ route, navigation }: MySpaceOuterPlanetProp):ReactElement {
   return (
     <View style={styles.container}>
       <Header route={route} navigation={navigation} />
--- utils/types.tsx
+++ utils/types.tsx
@@ -1,6 +1,40 @@
+import {BottomTabNavigationProp} from "@react-navigation/bottom-tabs";
 import { StackNavigationProp } from '@react-navigation/stack';
 import { DrawerNavigationProp } from '@react-navigation/drawer';
 import { RouteProp } from '@react-navigation/native';
+
+export type MainBottomTabNav = {
+  MySpace: undefined;
+  SpaceJourney: undefined;
+}
+
+export interface MainBottomTabMySpaceProp {
+  route: RouteProp<MainBottomTabNav, 'MySpace'>
+  navigation: BottomTabNavigationProp<MainBottomTabNav, 'MySpace'>
+}
+
+export interface MainBottomTabSpaceJourneyProp {
+  route: RouteProp<MainBottomTabNav, 'SpaceJourney'>
+  navigation: BottomTabNavigationProp<MainBottomTabNav, 'SpaceJourney'>
+}
+
+
+export type MySpaceStackNav = {
+  InPlanet: undefined;
+  OuterPlanet: undefined;
+  Feed: undefined;
+}
+
+export interface MySpaceInPlanetProp {
+  route: RouteProp<MySpaceStackNav, 'InPlanet'>
+  navigation: StackNavigationProp<MySpaceStackNav, 'InPlanet'>
+}
+
+export interface MySpaceOuterPlanetProp {
+  route: RouteProp<MySpaceStackNav, 'OuterPlanet'>
+  navigation: StackNavigationProp<MySpaceStackNav, 'OuterPlanet'>
+}
+
 
 export type JourneyGalaxyStackNav = {
   SelectGalaxy: undefined;
@@ -13,6 +47,7 @@ export interface JourneyGalaxyStackProp {
   route: RouteProp<JourneyGalaxyStackNav, 'JourneyGalaxy'>
   navigation: StackNavigationProp<JourneyGalaxyStackNav, 'JourneyGalaxy'>
 }
+
 
 export type OuterPlanetDrawerNav = {
   OuterPlanetMain: undefined;
