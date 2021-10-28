dfdbfdb8a779bf14516ec362b92d5b36a4e0f9c7
jiwon79 <19-079@ksa.hs.kr>
2021-10-29 04:25:07 +0900

    feat: Add WriteFeed InputImage Header

---
+++ components/Feed/WriteFeed/InputImageHeader.tsx
@@ -0,0 +1,34 @@
+import React, { ReactElement } from "react";
+import {View, Text, StyleSheet, TouchableOpacity } from "react-native";
+
+import EStyleSheet from "react-native-extended-stylesheet";
+import HeaderStyle from "../../atom/HeaderStyle";
+import styleGuide from "../../../utils/styleGuide";
+
+import Back from '../../../assets/icon/Back_white32.svg'
+import Next from '../../../assets/icon/Next_white32.svg'
+
+export default function InputImageHeader(): ReactElement {
+  return (
+    <View style={[HeaderStyle.header, styles.header]}>
+      <TouchableOpacity>
+        <Back/>
+      </TouchableOpacity>
+
+      <Text style={[HeaderStyle.title, styleGuide.display_05]}>
+        <Text style={styleGuide.font_light}>새 게시물-</Text>
+        사진 추가
+      </Text>
+
+      <TouchableOpacity>
+        <Next/>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: EStyleSheet.value('$darkGray_02')
+  }
+})
